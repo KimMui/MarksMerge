0201cb68093491402e9fca4f8df538dc8c0cd36d
KimMui <mui_kim@projectara.com>
2015-06-18 09:31:24 -0700

    Added Mixing

--- nuttx/configs/ara/svc/src/svc.c
+++ nuttx/configs/ara/svc/src/svc.c
@@ -109,6 +109,22 @@ struct svc_interface_device_id {
 #define DEMO_VIBRATOR_APB1_CPORT (2)
 #define DEMO_VIBRATOR_APB2_CPORT (3)
 
+
+#ifdef CONFIG_ARA_DUAL_I2S_PORTS
+#define DEMO_I2S_MGMT_APB1_CPORT_1 (4)
+#define DEMO_I2S_MGMT_APB2_CPORT_1 (9)
+#define DEMO_I2S_RX_APB1_CPORT_1   (5)
+#define DEMO_I2S_RX_APB2_CPORT_1   (10)
+#endif
+
+#ifdef CONFIG_ARA_STREAM_FROM_APB3_TO_APB2
+#define DEV_ID_APB3             (3)
+
+#define DEMO_I2S_MGMT_APB3_CPORT_1 (4)
+#define DEMO_I2S_RX_APB3_CPORT_1   (5)
+#endif
+
+
 /* Interface name to deviceID mapping table */
 static struct svc_interface_device_id devid[] = {
     { "apb1", DEV_ID_APB1 },
